7e96eb266c83c8102a9fae095e5bd90862b19bcc
Dmitrii Mazurok <tmazurok@gmail.com>
2023-06-13 12:26:25 +0300

    add searh service

---
+++ src/server/features/search/index.ts
@@ -0,0 +1,32 @@
+const getRandomNumber = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const getRandomFromArray = <T>(array: T[]) =>
+  array[Math.floor(Math.random() * array.length)];
+
+const flightCompanies = [
+  {
+    id: 1,
+    name: "Fly Emirates",
+  },
+  {
+    id: 2,
+    name: "Turkish Airlines",
+  },
+  {
+    id: 3,
+    name: "Virgin",
+  },
+];
+
+export const search = ({ from, to }: { from: string; to: string }) => {
+  const duration = getRandomNumber(60, 360);
+
+  return Array.from({ length: getRandomNumber(5, 10) }).map((_, index) => ({
+    id: index,
+    from,
+    to,
+    duration,
+    flightCompany: getRandomFromArray(flightCompanies),
+  }));
+};
